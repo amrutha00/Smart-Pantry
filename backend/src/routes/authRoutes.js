f7445d816e84eccdd0ae64ac2c6064f3004d36ee
Bala Sukesh Belide <bbelide2@illinois.edu>
2023-12-05 20:24:40 +0000

    Upload New File

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = express.Router();
+
+// Register route
+router.post('/register', async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const user = new User({ username, email, password });
+      await user.save();
+      res.status(201).send('User registered successfully');
+    } catch (error) {
+      if (error.code === 11000) {
+        // Duplicate key error
+        const field = Object.keys(error.keyPattern)[0];
+        return res.status(400).send(`An account with that ${field} already exists. Both username and email must be unique`);
+      }
+      res.status(500).send(error.message);
+    }
+  });
+
+// Login route
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !await bcrypt.compare(password, user.password)) {
+      return res.status(401).send('Invalid credentials');
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
