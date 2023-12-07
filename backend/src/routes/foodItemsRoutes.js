b826913efca856e033181f723bdcaa8ee1f23b68
Bala Sukesh Belide <bbelide2@illinois.edu>
2023-12-06 18:16:06 -0600

    Add timezone and other user apis

--- backend/src/routes/foodItemsRoutes.js
+++ backend/src/routes/foodItemsRoutes.js
@@ -1,50 +1,89 @@
 const express = require('express');
 const FoodItem = require('../models/FoodItem');
 const router = express.Router();
+const moment = require('moment-timezone');
+const admin = require('../../firebaseAdmin');
+const db = admin.firestore();
 
 // Middleware to verify user token
 const verifyToken = require('../middleware/verifyToken');
 
 // POST - Create a new food item
-router.post('/food-items', verifyToken, async (req, res) => {
-    try {
+router.post('/', verifyToken, async (req, res) => {
+  try {
       const { name, boughtDate, expiryDate } = req.body;
-      const foodItem = new FoodItem({ userId: req.user.user_id, name, boughtDate, expiryDate });
+      const userId = req.user.user_id;
+
+      // Fetch user's timezone from Firestore
+      const userRef = db.collection('users').doc(req.user.email);
+      const doc = await userRef.get();
+      const userData = doc.data();
+      const timezone = userData ? userData.timezone : 'UTC';
+      console.log(timezone);
+
+      // Check if the item is already expired
+      if (moment().tz(timezone).isAfter(moment(expiryDate))) {
+          return res.status(400).json({ message: "The item is already expired", data: {} });
+      }
+
+      const foodItem = new FoodItem({ userId, name, boughtDate, expiryDate });
       await foodItem.save();
       res.status(201).json({ message: "OK", data: foodItem });
-    } catch (error) {
+  } catch (error) {
       res.status(400).json({ message: error.message, data: {} });
-    }
-  });
-  
+  }
+});
 
 // GET - List all food items for a user
-router.get('/food-items', verifyToken, async (req, res) => {
+router.get('/', verifyToken, async (req, res) => {
     try {
+      // Fetch user's timezone from Firestore
+      const userRef = db.collection('users').doc(req.user.email);
+      const doc = await userRef.get();
+      const userData = doc.data();
+      const timezone = userData ? userData.timezone : 'UTC';
+      console.log(timezone);
+  
+      // Fetch food items from MongoDB
       const foodItems = await FoodItem.find({ userId: req.user.user_id });
-      res.status(200).json({ message: "OK", data: foodItems });
+      const transformedFoodItems = foodItems.map(item => {
+        const isExpired = isItemExpired(item, timezone);
+        return { ...item.toObject(), isExpired };
+      });
+  
+      res.status(200).json({ message: "OK", data: transformedFoodItems });
     } catch (error) {
       res.status(500).json({ message: error.message, data: {} });
     }
-  });
+});
   
 // PUT - Update a food item
-router.put('/food-items/:id', verifyToken, async (req, res) => {
-    try {
+router.put('/:id', verifyToken, async (req, res) => {
+  try {
       const update = req.body;
       const foodItem = await FoodItem.findOneAndUpdate({ _id: req.params.id, userId: req.user.user_id }, update, { new: true });
+
       if (!foodItem) {
-        return res.status(404).json({ message: "Food item not found", data: {} });
+          return res.status(404).json({ message: "Food item not found", data: {} });
       }
-      res.status(200).json({ message: "OK", data: foodItem });
-    } catch (error) {
+
+      // Fetch user's timezone from Firestore
+      const userRef = db.collection('users').doc(req.user.email);
+      const doc = await userRef.get();
+      const userData = doc.data();
+      const timezone = userData ? userData.timezone : 'UTC';
+
+      const updatedItem = { ...foodItem.toObject(), isExpired: isItemExpired(foodItem, timezone) };
+
+      res.status(200).json({ message: "OK", data: updatedItem });
+  } catch (error) {
       res.status(400).json({ message: error.message, data: {} });
-    }
-  });
-  
+  }
+});
+
 
 // DELETE - Delete a food item
-router.delete('/food-items/:id', verifyToken, async (req, res) => {
+router.delete('/:id', verifyToken, async (req, res) => {
     try {
       const foodItem = await FoodItem.findOneAndDelete({ _id: req.params.id, userId: req.user.user_id });
       if (!foodItem) {
@@ -54,7 +93,11 @@ router.delete('/food-items/:id', verifyToken, async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: error.message, data: {} });
     }
-  });
-  
+});
+
+
+function isItemExpired(item, timezone) {
+    return moment().tz(timezone).isAfter(moment(item.expiryDate));
+}
 
 module.exports = router;
